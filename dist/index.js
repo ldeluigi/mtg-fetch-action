27b7f0fa5b19ea05985b8f60d156327abcaeae78
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-12-26 20:51:05 +0100

    Apply changes

--- dist/index.js
+++ dist/index.js
@@ -36,7 +36,7 @@ function sendPricingInfo(card) {
             url: card.image_uris.png
         }
     };
-    return `### [${data.title}](${data.url})\n![${card.name}](${data.image.url})`;
+    return `### [${data.title}](${data.url})\n<img src="${data.image.url}" alt="${card.name}" width="200"/>`;
 }
 function sendLegalityInfo(card) {
     let legaityString = '';
@@ -58,7 +58,7 @@ function sendGathererInfo(card) {
             url: card.image_uris.png
         }
     };
-    return `### [${data.title}](${data.url})\n![${card.name}](${data.image.url})`;
+    return `### [${data.title}](${data.url})\n<img src="${data.image.url}" alt="${card.name}" width="200"/>`;
 }
 function sendEdhrecInfo(card) {
     const data = {
@@ -68,7 +68,7 @@ function sendEdhrecInfo(card) {
             url: card.image_uris.png
         }
     };
-    return `### [${data.title}](${data.url})\n![${card.name}](${data.image.url})`;
+    return `### [${data.title}](${data.url})\n<img src="${data.image.url}" alt="${card.name}" width="200"/>`;
 }
 function pickBest(cardName, cardList) {
     let max = Number.NEGATIVE_INFINITY;
