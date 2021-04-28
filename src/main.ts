29fe800880c693d2e75ecbbf780f69ae46d7a068
Luca Deluigi <lucadelu97@gmail.com>
2021-04-28 13:40:50 +0200

    Add answer splitting based on max length

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import * as core from '@actions/core'
 import * as bot from './bot-utils'
 import {context, getOctokit} from '@actions/github'
 
+const ANSWER_CHAR_LIMIT = 65535
+
 export async function run(): Promise<void> {
   try {
     const githubToken: string = core.getInput('github-token', {required: true})
@@ -72,27 +74,32 @@ export async function run(): Promise<void> {
           ? bot.printHelp()
           : bot.searchForCards(body))
         if (answer.length > 0) {
-          if (context.eventName === 'pull_request_review_comment') {
-            if (context.payload.pull_request && context.payload.comment) {
-              await githubClient.pulls.createReplyForReviewComment({
+          let answerLeftover: string = answer
+          while (answerLeftover.length > 0) {
+            const answerToSend = answerLeftover.substring(0, ANSWER_CHAR_LIMIT)
+            answerLeftover = answerLeftover.substring(ANSWER_CHAR_LIMIT)
+            if (context.eventName === 'pull_request_review_comment') {
+              if (context.payload.pull_request && context.payload.comment) {
+                await githubClient.pulls.createReplyForReviewComment({
+                  owner: context.repo.owner,
+                  repo: context.repo.repo,
+                  pull_number: context.payload.pull_request.number,
+                  comment_id: context.payload.comment.id,
+                  body: answerToSend
+                })
+              } else {
+                core.warning(
+                  'Could not reply to review comment because pull_request number or comment id are missing.'
+                )
+              }
+            } else {
+              await githubClient.issues.createComment({
+                issue_number: context.issue.number,
                 owner: context.repo.owner,
                 repo: context.repo.repo,
-                pull_number: context.payload.pull_request.number,
-                comment_id: context.payload.comment.id,
-                body: answer
+                body: answerToSend
               })
-            } else {
-              core.warning(
-                'Could not reply to review comment because pull_request number or comment id are missing.'
-              )
             }
-          } else {
-            await githubClient.issues.createComment({
-              issue_number: context.issue.number,
-              owner: context.repo.owner,
-              repo: context.repo.repo,
-              body: answer
-            })
           }
         }
       } catch (error) {
